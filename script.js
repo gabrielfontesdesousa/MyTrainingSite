2a5acd28cbac84c7923a1ceb8d86b9480489fddb
Gabriel Fontes de Sousa <143654496+gabrielfontesdesousa@users.noreply.github.com>
2025-08-10 21:26:15 -0300

    alterando treino

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const inputs = document.querySelectorAll('.weight-input');
-
   inputs.forEach((input, index) => {
     const savedValue = localStorage.getItem(`weightInput-${index}`);
     if (savedValue) {
       input.value = savedValue;
     }
-
     input.addEventListener('blur', () => {
       localStorage.setItem(`weightInput-${index}`, input.value);
     });

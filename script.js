ed8caac60eb876891496cc0c46aca08ea7c7ef4a
Gabriel Fontes de Sousa <143654496+gabrielfontesdesousa@users.noreply.github.com>
2025-08-14 20:23:54 -0300

    adicionando input de reps

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ const inputs = document.querySelectorAll('.weight-input');
       localStorage.setItem(`weightInput-${index}`, input.value);
     });
   });
+
+  // Salvar e carregar CARGA
+const weightInputs = document.querySelectorAll('.weight-input');
+weightInputs.forEach((input, index) => {
+  const savedValue = localStorage.getItem(`weightInput-${index}`);
+  if (savedValue) {
+    input.value = savedValue;
+  }
+  input.addEventListener('blur', () => {
+    localStorage.setItem(`weightInput-${index}`, input.value);
+  });
+});
+
+// Salvar e carregar REPS
+const repsInputs = document.querySelectorAll('.reps-input');
+repsInputs.forEach((input, index) => {
+  const savedValue = localStorage.getItem(`repsInput-${index}`);
+  if (savedValue) {
+    input.value = savedValue;
+  }
+  input.addEventListener('blur', () => {
+    localStorage.setItem(`repsInput-${index}`, input.value);
+  });
+});
+
